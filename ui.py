4fddc078f2e02c1823001e07dc2dedefb636a8f2
Shaji Khan <git@shaji.in>
2023-07-14 19:35:38 +0530

    home folder

--- ui.py
+++ ui.py
@@ -1,13 +1,28 @@
-import sys
+import sys, os
+import glob
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
+from gi.repository.GdkPixbuf import Pixbuf
 
 class UI(Gtk.ApplicationWindow):
     box = None
     iconSize = 64
-    
+    icons = ["edit-cut", "edit-paste", "edit-copy"]
+
+    def openHome (self, data = None):
+        self.openFolder (os.path.expanduser ("~"))
+
+    def openFolder (self, path):
+        files = glob.glob (path + "/*")
+        # print (files)
+        self.liststore.clear ()
+        for f in files:
+            if os.path.isdir (f):
+                pixbuf = Pixbuf.new_from_file ("icons/folder.svg")
+                self.liststore.append ([pixbuf, os.path.basename (f), f])
+
     def button (self, filename):
         image = Gtk.Image.new_from_file (filename)
         button = Gtk.Button (child=image)
@@ -31,6 +46,7 @@ class UI(Gtk.ApplicationWindow):
         self.header.pack_start (back)
         home = self.button ("icons/go-home.svg")
         self.header.pack_start (home)
+        home.connect ("clicked", self.openHome)
 
         self.bar = Gtk.Entry ()
         self.bar.set_hexpand (True)
@@ -38,3 +54,14 @@ class UI(Gtk.ApplicationWindow):
         
         go = self.button ("icons/go-next.svg")
         self.header.pack_end (go)
+
+        self.iconview = Gtk.IconView ()
+        self.sw = Gtk.ScrolledWindow ()
+        self.sw.set_child (self.iconview)
+        self.liststore = Gtk.ListStore(Pixbuf, str, str)
+        self.iconview.set_model(self.liststore)
+        self.iconview.set_pixbuf_column(0)
+        self.iconview.set_text_column(1)
+        self.box.append (self.sw)
+        self.sw.set_hexpand (True)
+        self.sw.set_vexpand (True)
